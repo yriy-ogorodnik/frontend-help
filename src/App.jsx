b34999319f9cd16129e88bb64253fd778f631c4f
Yriy Ogorodnik <yriyelektric@gmail.com>
2024-01-05 23:23:14 +0200

    add lib

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,20 @@ function App() {
   return (
     <>
       <div>
-      <Barron/>
+      <Barron />
        
         
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="card text-3xl font-bold underline ">
+        <button className="card text-3xl font-bold underline shadow-inner shadow md:shadow-lg "  onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-3xl font-bold underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
